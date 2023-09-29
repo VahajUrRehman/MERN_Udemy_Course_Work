9ea3a8a4a1074521c7f86a6c73d95e6270d12dc0
Muhammad Vahaj Ur Rehman <vahaj2000@outlook.com>
2023-09-30 01:43:35 +0500

    The  New Dice game Has Been Created

---
+++ The Dice Game/index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+var sourceUrl1 = "images/dice" + randomNumber1 + ".png";
+console.log(randomNumber1);
+document.querySelector(".dice .img1").setAttribute("src", sourceUrl1);
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+var sourceUrl2 = "images/dice" + randomNumber2 + ".png";
+
+console.log(randomNumber2);
+document.querySelector(".dice .img2").setAttribute("src", sourceUrl2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "🚩Player 1 Wins";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").textContent = "Player 2 Wins🚩";
+} else {
+  document.querySelector("h1").textContent = "Draw!🏁";
+}
