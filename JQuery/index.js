384bc04f60588d7c6e52371d069665a70dbfafaa
Muhammad Vahaj Ur Rehman <vahaj2000@outlook.com>
2023-10-01 12:23:32 +0500

    Learned Jquery, manupuilating Styles, Attributes ,text Adnd Adding Event Listners

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,1 +1,36 @@
-$("h1").css("color", "orange");
+//Style MAnupulation
+// $("h1").css("color", "orange");
+// $("h1").addClass("big-title");
+// $("h1").hasClass("big-title");
+// $("h1").removeClass("big-title");
+
+//text Manupulation
+// $(".btn").text("Now Click Me");
+$(".btn").html("<em>Click Me</em>");
+
+//Attributes Manupulation
+// console.log($("a").attr("href"));
+$("a").attr("href", "https://www.bing.com");
+
+//Adding Event Listeners In JQuery
+
+$(".btn").click(function () {
+  $("h1").css("color", "green");
+});
+
+$("input").keydown(function (event) {
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "orange");
+});
+
+//Adding And removing Elements Using Jquery
+
+$("h1").before("<button> Before </button>"); //Adding Button out of h1 Before
+$("h1").after("<button> After </button>"); //Adding Button out of h1  After
+$("h1").append("<button> Append </button>"); //Adding Button in h1 The After The text
+$("h1").prepend("<button> Prepend </button>"); //Adding Button in h1 The Before The text
+
+// $("button").remove(); //This Would remove All Buuttons In Document
