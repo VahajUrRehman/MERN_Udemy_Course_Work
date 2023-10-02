27c72075fcc142fdd93dcb2f88036505b94bb10a
Muhammad Vahaj Ur Rehman <vahaj2000@outlook.com>
2023-10-02 07:09:38 +0500

    Learned About POSTMAN

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -4,19 +4,45 @@ const app = express();
 
 const port = 5000;
 
+// app.get("/", (req, res) => {
+//   res.send("<h1>Home Page</h1>");
+//   //   res.send("<h1>Hello World from Express Js</h1>");
+// });
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About Us</h1>");
+//   //   res.send("<h1>Hello World from Express Js</h1>");
+// });
+// app.get("/contact", (req, res) => {
+//   res.send("<h1>Contact US</h1>");
+//   //   res.send("<h1>Hello World from Express Js</h1>");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening Port ${port}`);
+// });
+
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
-  //   res.send("<h1>Hello World from Express Js</h1>");
 });
-app.get("/about", (req, res) => {
-  res.send("<h1>About Us</h1>");
-  //   res.send("<h1>Hello World from Express Js</h1>");
+
+app.post("/register", (req, res) => {
+  //Do something with the data
+  res.sendStatus(201);
 });
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact US</h1>");
-  //   res.send("<h1>Hello World from Express Js</h1>");
+
+app.put("/user/vahaj", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/vahaj", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/vahaj", (req, res) => {
+  //Deleting
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Listening Port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
